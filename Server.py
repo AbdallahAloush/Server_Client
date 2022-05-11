fda038cbb65ab012f569130e2970431f7db62e5a
AbdallahAloush <abdallahaloush7@gmail.com>
2022-05-11 22:18:47 +0200

    accept and listen to clients

--- Server.py
+++ Server.py
@@ -1,11 +1,21 @@
 import socket
 import selectors
 import sys
+import types
 
 #TODO: implement this as a bash argument when running the code
 listenaddr = ('127.0.0.1', 8080)
 sel = selectors.DefaultSelector()
 
+def accept_wrapper(sock):
+    server_socket, client_socket = sock.accept()
+    print(f'Accepted request from client {client_socket}\n')
+    server_socket.setblocking(False)
+    data = types.SimpleNamespace(addr=client_socket, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(server_socket, events, data=data)
+
+    
 # Initiating the listening socket 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.bind(listenaddr)
@@ -14,13 +24,21 @@ print(f'Server started listeninig on {listenaddr[0]}: {listenaddr[1]}')
 listen_socket.setblocking(False)
 sel.register(listen_socket, selectors.EVENT_READ, data=None)
 
+
+try:
 # The event loop
-while True:
-    events = sel.select(timeout= None)
-    for key, mask in events:
-        if key.data is None:
-            # accept connection and process it
-        else:
-            # service the connectoin
-    # handle the events at the selector object
-    print(events)
+    while True:
+        events = sel.select(timeout= None)
+        for key, mask in events:
+            if key.data is None:
+                # accept connection and process it
+                accept_wrapper(key.fileobj)
+            else:
+                # service the connectoin
+                print(key.data)
+                sys.exit()
+        # handle the events at the selector object
+except KeyboardInterrupt:
+    sys.exit()
+
+
