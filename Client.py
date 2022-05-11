1e68592ee5925c920f8fa1599ae0a611ac86b7b3
Mazen Abouelhassen <mazenabouelhassen@gmail.com>
2022-05-11 22:41:34 +0200

    Added generate operations list function and created the genereate http request function

--- Client.py
+++ Client.py
@@ -2,6 +2,7 @@ from calendar import c
 import socket
 import selectors
 import sys
+from urllib import request
 
 def generate_operations_list(filename):
     operations =[]
@@ -12,7 +13,28 @@ def generate_operations_list(filename):
             print(anOperation, end='')  
     return operations
 
-def  
+def construct_http_request(operation):
+    operations_elements = operation.split(' ')
+    request_method = operations_elements[0]
+    file_name = operations_elements[1]
+    host_name = operations_elements[2]
+    if len(operations_elements) == 4:
+        port_number = operations_elements[3]
+    else:
+        port_number = "80"
+    #print(operations_elements)
+
+
+construct_http_request("GET /hypertext/WWW/TheProject.html info.cern.ch 80")
+
+ 
+    
+
+
+
+
+    
+
    
 
 #generate_operations_list("input_file.txt")
