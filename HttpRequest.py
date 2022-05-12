933a495bf3c46f7eb1a9fef3b9a5516be82eaa32
AbdallahAloush <abdallahaloush7@gmail.com>
2022-05-12 13:36:20 +0200

    Code refactor

--- HttpRequest.py
+++ HttpRequest.py
@@ -1,5 +1,7 @@
 import selectors
 
+recv_buffer_size = 10000
+
 class request:
     
     def __init__(self, socket, selector, address):
@@ -10,9 +12,11 @@ class request:
         self.selector = selector
         self.socket = socket
         self.address = address
+        self.message = ''           # ASCII decoded message
         self.recv_buffer = b''
         self.send_buffer = b''
         self.requestSize = 0            # total size of the request in bytes
+        self.header_fields = dict()
 
 
     def process_events(self, mask):
@@ -23,19 +27,38 @@ class request:
             pass
     
     def readRequest(self):
-        self.recv_buffer = self.socket.recv(10000)
-        self.requestSize = len(self.recv_buffer)
-        self.message = self.recv_buffer.decode('ascii')
-        self.message.find('GET')
-        print('get request')
-        self.processGET()
+        self.recv_buffer = self.socket.recv(recv_buffer_size)
+        self.requestSize = len(self.recv_buffer)     # will use this to check that we have the complete message by comparing it to conten length in post requests
+        self.message += self.recv_buffer.decode('ascii')   
+        print(self.message)
+        header = self.message.partition('\r\n\r\n')[0]
+        requestLine = self.message.partition('\r\n')[0]
+        method = requestLine.split(' ')[0]
+        URL = requestLine.split(' ')[1][1:]
+        version = requestLine.split(' ')[2]        #HTTP version
+        
+        for header_line in header.split('\r\n')[1:]:
+            line_list = header_line.split(': ')
+
+            self.header_fields[line_list[0]] = line_list[1]
+
+        if method == 'GET':
+            self.processGET(URL)
+        if method == 'POST':
+            self.processPOST(URL)
         self.selector.modify(self.socket, selectors.EVENT_WRITE, data=self)
-        print('reading now')
-    
-    def processGET(self):
-        temp = self.message.split(' ')
-        filename = (temp[1])[1:]
+
+
+    def processGET(self, filename):
         print(filename)
+        print(f'\n\n\n\n\n{self.header_fields}')
         f = open(filename, "r")
         print(f.read())
-        
+
+    def processPOST(self, filename):
+        print(filename)
+        self.message = self.message.partition('\r\n\r\n')
+        print(self.message[1])
+        body = self.message[-1]
+        print(body)
+    
--- Server.py
+++ Server.py
@@ -1,40 +1,45 @@
 import socket
 import selectors
-import types
 import sys
-
-from requests import request
 import HttpRequest
 
 localhost = '127.0.0.1'
 port = 8080
+selector = selectors.DefaultSelector()
+listening_address = (localhost, port)
 
 def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    message = HttpRequest.request(conn, sel, addr)
-    sel.register(conn, selectors.EVENT_READ, data=message)
+    server_socket, client_address = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {client_address}")
+    server_socket.setblocking(False)
+    message = HttpRequest.request(server_socket, selector, client_address)
+    selector.register(server_socket, selectors.EVENT_READ, data=message)
 
-sel = selectors.DefaultSelector(    )
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((localhost, port))
-lsock.listen()
-print(f"Listening on {(localhost, port)}")
-lsock.setblocking(False)
-events = selectors.EVENT_READ
-sel.register(lsock,events, data=None)
+def eventLoop():
+    try:
+        while True:
+            events = selector.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    print('initiate connection')
+                    accept_wrapper(key.fileobj)
+                else:
+                    key.data.process_events(mask)
+    except KeyboardInterrupt:
+        sys.exit()
 
-# event loop 
-try:
-    while True:
-        events = sel.select(timeout= None)
-        for key, mask in events:
-            if key.data is None:
-                print('initiate connection')
-                accept_wrapper(key.fileobj)
-            else:
-                key.data.process_events(mask)
+# Entry point of the program
+def main():
+    # Initiating the listening socket
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.bind(listening_address)
+    lsock.listen()
+    print(f"Server started listening on localhost:{listening_address[1]}")
+    lsock.setblocking(False)
+    # Registering the listening socket on the selector
+    selector.register(lsock, selectors.EVENT_READ, data=None)
+    eventLoop()
 
-except KeyboardInterrupt:
-    sys.exit()
+# Starting the main function of the program
+if __name__ == "__main__":
+    main()
