e73118a53755e0d6b7051b4476f21ea8adf1ffe0
Mazen Abouelhassen <mazenabouelhassen@gmail.com>
2022-05-13 20:47:03 +0200

    Successfully completed the Client file and properly adjusted caching

---
+++ Cached/README.md
@@ -0,0 +1,27 @@
+# Server Client
+
+This project using multithreading, pipelining and HTTP/1.1 to make a stripped down version of HTTP Client and Server
+
+## Server features:
+
+- MultiThreading
+
+- Persistant connections
+
+## Client features:
+
+- Caching repeated requests
+
+## Running the server:
+
+```bash
+cd Server
+python Server.py
+```
+
+## Running the client:
+
+```bash
+python Client.py
+```
+
--- Client.py
+++ Client.py
@@ -16,9 +16,7 @@ def generate_operations_list(filename):
     with open(f'{filename}','rt') as inputFile:
         for anOperation in inputFile:
             operations.append(anOperation)
-    #for anOperation in operations:
-        #    print(anOperation, end='')  
-    return operations #returns lsit of operations
+    return operations                                       #returns list of operations
 
 
 #This functions extracts the main elements of http request from operation
@@ -26,24 +24,23 @@ def extract_operation_elements(operation):
     operations_elements = operation.split(' ')
     request_method = operations_elements[0]
     requested_file_name = operations_elements[1]
-    if len(operations_elements) == 4 and operations_elements[3] != '' :
+    if len(operations_elements)== 4:
         host_name = operations_elements[2]
         port_number = int(operations_elements[3][:-1])
-        #print (f'{port_number}')
-        #print(type(port_number))
-    else:
+    elif len(operations_elements)==3:
         host_name = operations_elements[2][:-1]
         port_number = 80
     return request_method, requested_file_name, host_name, port_number
 
+
 #This function constructs the http request in the right format
-def construct_http_request(method,file,host,port):
+def construct_http_request(method,fileName,host,port):
     if method == "GET":
-        http_request = f'{method} {file} HTTP/1.0\r\nHost:{host}:{port}\r\n\r\n'
+        http_request = f'{method} {fileName} HTTP/1.0\r\nHost:{host}:{port}\r\n\r\n' 
     else:   #method = "POST"
-        with open(f'{file}','r') as file:
-            fileCotents = file.read()
-        http_request = f'{method} {file} HTTP/1.0\r\nHost:{host}:{port}\r\n\r\n{fileCotents}\r\n'
+        with open(f'{fileName}','r') as file_object:
+            fileCotents = file_object.read()
+        http_request = f'{method} /{fileName} HTTP/1.0\r\nHost:{host}:{port}\r\n\r\n{fileCotents}\r\n'
     return http_request
     
 def convert_request_bytes(http_request):
@@ -57,8 +54,8 @@ def extract_message_code(server_response):
 def handle_successful_GET(server_response,filename): #HTTP/1.0 200 OK\r\n\r\ndatadatadata\r\n
     temp = server_response.partition('\r\n\r\n')
     received_data = temp[2]
-    cache_path = '\Cached'
-    cached_file_name = os.path.join(cache_path, filename)
+    cache_path = 'Cached'
+    cached_file_name = cache_path+filename
     #File object to write the file contents in the cache
     output_file_cache = open(cached_file_name, "w")
     output_file_cache.write(received_data)
@@ -68,9 +65,10 @@ def handle_successful_GET(server_response,filename): #HTTP/1.0 200 OK\r\n\r\ndat
     output_file.write(received_data)
     output_file.close()
 
-def handle_successful_cache_hit(cached_file_path,original_file_name):  #\Cached\Mazen.txt
+def handle_successful_cache_hit(cached_file_path,original_file_name):  #Cached\Mazen.txt
     cached_file_read = open(cached_file_path, "r")
     cached_file_contents = cached_file_read.read()
+    #!print("_____________________________________\n"+cached_file_contents+"________________________________________\n")
     cached_file_write = open(original_file_name,'w')
     cached_file_write.write(cached_file_contents)
     cached_file_read.close()
@@ -85,41 +83,40 @@ def isCached(requests_list,http_request):
 def main():
     operations_list = generate_operations_list("input_file.txt")
     cached_requests_list = []
-    for i in range (0,len(operations_list)-1):
-        conn_id = i+1                                                    #assigning an id to each operation to access an individual operation for later usage
+    for i in range (0,len(operations_list)):
+        conn_id = i                                                    #assigning an id to each operation
         an_operation = operations_list[conn_id]
-        method, file, host, port = extract_operation_elements(an_operation)
+        method, fileName, host, port = extract_operation_elements(an_operation)
         #Composing the request packet
-        http_request = construct_http_request(method,file,host,port)
+        http_request = construct_http_request(method,fileName,host,port)
         print("Http Request--->"+http_request+'\n')
-        if isCached(cached_requests_list,http_request) ==1:
+        if isCached(cached_requests_list,http_request) ==1:             #Check if  http request is cached(no need to connect to server)
             print("Cache hit successful!\n")
-            handle_successful_cache_hit(f'\Cached{file}',file)
-        else:
-            print("Cache hit not successful , connecting to server!\n")
+            handle_successful_cache_hit(f'Cached{fileName}',fileName)
+        else:                                                           #Request is not cached, proceed to connect to server
+            if method =="GET":
+                print("Cache hit not successful , connecting to server!\n")  
             http_request_in_bytes = convert_request_bytes(http_request)
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             client.connect((host,port))
             client.sendall(http_request_in_bytes)
             while True:                        #event loop waiting for the server's response
                 server_response_inBytes = client.recv(1024)
-                server_response = server_response_inBytes.decode('ascii')        #keep the decoded recieved data from server in a variable for later use if needed
+                server_response = server_response_inBytes.decode('ascii')      #keep the decoded recieved data from server in a variable for later use if needed
                 print("Server Response:\n" ,server_response)
-                if method == "GET" and extract_message_code(server_response) =="404":         #HTTP/1.0 404 Not Found\r\n
+                if method == "GET" and extract_message_code(server_response) =="404":         
                     break
-                elif method =="GET" and extract_message_code(server_response) == "200":      #HTTP/1.0 200 OK\r\n\r\ndatadatadata\r\n
-                    handle_successful_GET(server_response,file)
-                    #!list bt add el opertaions
-                    cached_requests_list.append(http_request)
+                elif method =="GET" and extract_message_code(server_response) == "200":      
+                    handle_successful_GET(server_response,fileName)
+                    cached_requests_list.append(http_request)                   #A list to keep track of cached requests that have been fullfilled before
                     break
-                
-                else:                                              #method is POST and the client does nothing on both OK snd Forbidden responses
+                else:                               #method is POST and the client does nothing on both OK snd Forbidden responses
                     if extract_message_code(server_response) == "403":
                         print("Status of Post Request------> Forbidden \n")
                     else: 
                         print("Status of Post Request ------> accepted \n")
                     break
-            print("_______________________________________________________________________________________________")
+            print("______________________________________________________________________________________________________________________")
             client.close()
         
     
@@ -127,12 +124,6 @@ if __name__ =="__main__":
         main()
 
 
-#generate_operations_list("input_file.txt")
 
-# post ->HTTP/1.0 200 OK\r\n nag7a 
-        #HTTP/1.0 403 Forbidden\r\n
-
-# GET ->HTTP/1.0 200 OK\r\n\r\ndatadatadata\r\n
-        #HTTP/1.0 404 Not Found\r\n
 
 
